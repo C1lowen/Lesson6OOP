b20525274c7fbbc6fe6a9fc11765c71d77a04924
C1lowen <genukr5@ukr.net>
2023-08-07 18:13:21 +0300

    Add dz

--- src/lesson6_task2/Main.java
+++ src/lesson6_task2/Main.java
@@ -2,62 +2,111 @@ package lesson6_task2;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Random rn = new Random();
-        int[] arr = new int[1000000];
+        Scanner sc = new Scanner(System.in);
 
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = rn.nextInt(20, 100);
-        }
+        System.out.println("Введите размер масива:");
+        int size = sc.nextInt();
 
+        int[] arr = new int[size];
 
-        long start = System.nanoTime();
+        initializationMassive(arr);
 
-        SumMassive first = new SumMassive(arr, 0, 250000);
-        SumMassive second = new SumMassive(arr, 250000, 500000);
-        SumMassive third = new SumMassive(arr, 500000, 750000);
-        SumMassive fourth = new SumMassive(arr, 750000, 1000000);
+        System.out.println("Введите количество потоков(должно быть кратно размеру масива)");
+        int n = sc.nextInt();
 
-        Thread th1 = new Thread(first);
-        Thread th2 = new Thread(second);
-        Thread th3 = new Thread(third);
-        Thread th4 = new Thread(fourth);
+        checkValid(size, n);
 
-        th1.start();
-        th2.start();
-        th3.start();
-        th4.start();
+        SumMassive[] sumMassives = new SumMassive[n];
+        Thread[] threads = new Thread[n];
 
-        try {
-            th1.join();
-            th2.join();
-            th3.join();
-            th4.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        long end = System.nanoTime();
+        int elementOneStream = arr.length / 4;
 
-        long resultSum = first.getSum() + second.getSum() + third.getSum() + fourth.getSum();
+        long start = System.currentTimeMillis();
+
+        creationObjects(sumMassives, arr,elementOneStream);
+
+        creationStream(threads, sumMassives);
+
+        startingThreads(threads);
+
+        long end = System.currentTimeMillis();
+
+        long resultSum = sum(threads, sumMassives);
 
         System.out.println("Сума массива: " + resultSum);
-        System.out.println("Время выполнения: " + (end - start));
+        System.out.println("Время выполнения: " + (end - start) + " ms");
 
-        long start2 = System.nanoTime();
+        System.out.println("Обычный алгоритм:");
 
-        int sum = 0;
+        long start2 = System.currentTimeMillis();
+
+        long sum = 0;
 
         for(int i = 0; i < arr.length; i++){
             sum += arr[i];
         }
 
-        long end2 = System.nanoTime();
+        long end2 = System.currentTimeMillis();
 
         System.out.println("Сума массива: " + sum);
-        System.out.println("Время выполнения: " + (end2 - start2));
+        System.out.println("Время выполнения: " + (end2 - start2) + " ms");
 
 
     }
+
+    private static void initializationMassive(int[] arr){
+        Random rn = new Random();
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = rn.nextInt(0, 10);
+        }
+    }
+
+    private static void checkValid(int size, int n){
+         if(size % n != 0){
+             System.out.println("Такое количество потоков не может существовать!");
+             System.out.println("Программа не может быть выполнена коректно!");
+             System.exit(0);
+         }
+    }
+
+    private static void creationObjects(SumMassive[] sumMassives, int[] arr , int elementOneStream){
+        int begin = 0;
+        int ending = elementOneStream;
+
+        for(int i = 0; i < sumMassives.length; i++){
+            sumMassives[i] = new SumMassive(arr, begin, ending);
+            begin = ending;
+            ending += elementOneStream;
+        }
+    }
+
+    private static void creationStream(Thread[] threads, SumMassive[] sumMassives){
+        for(int i = 0; i < threads.length; i++){
+            threads[i] = new Thread(sumMassives[i]);
+            threads[i].start();
+        }
+    }
+
+    private static void startingThreads(Thread[] threads){
+        try {
+            for(int i = 0; i < threads.length; i++){
+                threads[i].join();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int sum(Thread[] threads ,SumMassive[] sumMassives){
+        int result = 0;
+        for(int i = 0; i< threads.length; i++){
+             result += sumMassives[i].getSum();
+        }
+        return result;
+    }
+
 }
